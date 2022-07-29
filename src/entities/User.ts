0dff4bd85e6ff6ad6ffd5ed5b7cde47e2f415156
Luan <luanoliveira17@hotmail.com>
2022-07-29 13:00:14 -0300

    Criptografando senha

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4'
+import { v4 } from 'uuid'
 
 export class User {
   public readonly id: string
@@ -11,6 +11,6 @@ export class User {
   constructor(props: Omit<User, 'id'>, id?: string) {
     Object.assign(this, props)
 
-    if (!id) this.id = uuid()
+    if (!id) this.id = v4()
   }
 }
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -2,6 +2,7 @@ import { User } from '../../entities/User'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 import { ICreateUserRequestDTO } from './CreateUserDTO'
 import { CreateUserValidator } from './CreateUserValidator'
+import sha256 from 'crypto-js/sha256'
 
 export class CreateUserUseCase {
   constructor(
@@ -16,6 +17,7 @@ export class CreateUserUseCase {
     if (userAlreadyExists) throw new Error('User already exists')
 
     const user = new User(data)
+    user.password = sha256(user.password).toString()
     await this.usersRepository.save(user)
   }
 }
